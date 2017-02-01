cd5c57eb08308a3ea820114d91a90c50899c7755
Elcior Santana <elciorfscarvalho@gmail.com>
2017-02-01 09:48:15 -0300

    Upload manual de arquivos originais do CodeIgniter

---
+++ application/controllers/Blog.php
@@ -0,0 +1,17 @@
+<?php
+class Blog extends CI_Controller
+{
+	public function index()
+	{
+		$data['title'] = 'Meu Blog CodeIgniter';
+		$data['page_title'] = 'Olá Blog CodeIgniter';
+		$data['todo_list'] = array('Clean House', 'Call Mom', 'Run Errands'); 
+
+		$this->load->view('blogview', $data);
+	}
+
+	public function welcome()
+	{
+		echo "<h1>Bem-vindo!</h1>";
+	}
+}
---
+++ application/views/blogview.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+</head>
+<body>
+
+	<h1><?php echo $page_title; ?></h1>
+
+	<ol>
+	<?php foreach ($todo_list as $item): ?>
+
+		<li><?php echo $item; ?></li>
+
+	<?php endforeach; ?>
+	</ol>
+
+</body>
+</html>
